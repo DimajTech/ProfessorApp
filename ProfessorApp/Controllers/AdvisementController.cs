4b508e6c6977a82479371c3875cfb4039cc20374
AxelSV50 <axelsv50@gmail.com>
2025-02-25 08:21:15 -0600

    Se asignan las URL desplegadas y el dockerfile. Se reestructuran las carpetas

---
+++ ProfessorApp/Controllers/AdvisementController.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using StudentApp.Models.DAO;
+using StudentApp.Models.Entity;
+
+namespace StudentApp.Controllers
+{
+    public class AdvisementController : Controller
+    {
+        private readonly ILogger<AdvisementController> _logger;
+        private readonly IConfiguration _configuration;
+
+        AdvisementDAO advisementDAO;
+        readonly string API_URL;
+        public AdvisementController(ILogger<AdvisementController> logger, IConfiguration configuration)
+        {
+
+            _logger = logger;
+            _configuration = configuration;
+            API_URL = _configuration["EnvironmentVariables:API_URL"];
+            advisementDAO = new AdvisementDAO(_configuration);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAdvisementById([FromQuery] string id)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                  
+                    var response = await client.GetAsync($"{API_URL}/api/Advisement/GetAdvisementById/{id}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var advisement = await response.Content.ReadFromJsonAsync<Advisement>();
+                        return Ok(advisement);
+                    }
+                    else
+                    {
+                        return StatusCode((int)response.StatusCode, new { message = "Error fetching advisement", error = response.ReasonPhrase });
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                return StatusCode(500, new { message = "Request error", error = e.Message });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = "Unexpected error", error = e.Message });
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAdvisementsByUser([FromQuery] string email)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                   
+                    var response = await client.GetAsync($"{API_URL}/api/Advisement/GetMyAdvisements/{email}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var advisements = await response.Content.ReadFromJsonAsync<List<Advisement>>();
+                        return Ok(advisements);
+                    }
+                    else
+                    {
+                        return StatusCode((int)response.StatusCode, new { message = "Error fetching advisements", error = response.ReasonPhrase });
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                return StatusCode(500, new { message = "Request error", error = e.Message });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = "Unexpected error", error = e.Message });
+            }
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetPublicAdvisements([FromQuery] string email)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+               
+                    var response = await client.GetAsync($"{API_URL}/api/Advisement/GetPublicAdvisements/{email}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var advisements = await response.Content.ReadFromJsonAsync<List<Advisement>>();
+                        return Ok(advisements);
+                    }
+                    else
+                    {
+                        return StatusCode((int)response.StatusCode, new { message = "Error fetching public advisements", error = response.ReasonPhrase });
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                return StatusCode(500, new { message = "Request error", error = e.Message });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = "Unexpected error", error = e.Message });
+            }
+        }
+
+
+        public IActionResult GetAdvisementResponsesById(string id)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(API_URL);
+
+                    var getTask = client.GetAsync($"/api/Advisement/GetResponsesByAdvisement/{id}");
+
+                    getTask.Wait();
+
+                    var result = getTask.Result;
+
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<List<ResponseAdvisement>>();
+                        readTask.Wait();
+
+                        return Ok(readTask.Result);
+                    }
+                    else
+                    {
+                        return StatusCode((int)result.StatusCode, new { message = "Error getting responses" });
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                return StatusCode(500, new { message = "Server error", error = e.Message });
+            }
+        }
+        [HttpPost]
+        public IActionResult AddNewResponse([FromBody] ResponseAdvisement response)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(API_URL);
+
+
+                    var postTask = client.PostAsJsonAsync("/api/Advisement/CreateResponseAdvisement", new
+                    {
+                        AdvisementId = response.AdvisementId,
+                        UserId = response.User.Id,
+                        Text = response.Text
+                    });
+
+                    postTask.Wait();
+
+                    var result = postTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<ResponseAdvisement>();
+                        readTask.Wait();
+                        return Ok(readTask.Result);
+                    }
+                    else
+                    {
+                        return StatusCode((int)result.StatusCode, new { message = "Failed to add response" });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = "An error occurred", error = e.Message });
+            }
+        }
+
+    }
+}
---
+++ ProfessorApp/Controllers/AppointmentController.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using ProfessorApp.DTO;
+using StudentApp.Models.DAO;
+using StudentApp.Models.Entity;
+
+namespace StudentApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly ILogger<AppointmentController> _logger;
+        private readonly IConfiguration _configuration;
+
+        private readonly string API_URL;
+
+        public AppointmentController(ILogger<AppointmentController> logger, IConfiguration configuration)
+        {
+
+            _logger = logger;
+            _configuration = configuration;
+
+            API_URL = _configuration["EnvironmentVariables:API_URL"];
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IEnumerable<Appointment> GetAppointments([FromBody] AppointmentsFilterDTO data)
+        {
+            IEnumerable<Appointment> appointments = null;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri($"{API_URL}/api/Appointment/GetAppointments");
+
+                    var postTask = client.PostAsJsonAsync("GetAppointments", data);
+                    postTask.Wait();
+
+                    var result = postTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<List<Appointment>>();
+                        readTask.Wait();
+                        //lee el estudiante provenientes de la API
+                        appointments = readTask.Result;
+                    }
+
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError(string.Empty, "Server error. Please contact an administrator");
+
+            }
+            return appointments;
+        }
+
+        [HttpGet]
+        [Route("Appointment/GetReviewedAppointments/{professorId}")]
+        public IEnumerable<Appointment> GetReviewedAppointments(string professorId)
+        {
+            IEnumerable<Appointment> appointments = null;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(API_URL);
+
+                    var getTask = client.GetAsync($"/api/Appointment/GetReviewedAppointments/{professorId}");
+                    getTask.Wait();
+
+                    var result = getTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<List<Appointment>>();
+                        readTask.Wait();
+                        // Lee los datos provenientes de la API
+                        appointments = readTask.Result;
+                    }
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError(string.Empty, "Server error. Please contact an administrator.");
+            }
+
+            return appointments;
+        }
+
+        [HttpGet]
+        [Route("Appointment/GetAppointmentById/{appointmentID}")]
+        public IActionResult GetAppointmentById(string appointmentID)
+        {
+            Appointment appointment = null;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(API_URL);
+
+                    var getTask = client.GetAsync($"/api/Appointment/GetAppointmentById/{appointmentID}");
+                    getTask.Wait();
+
+                    var result = getTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<Appointment>();
+                        readTask.Wait();
+                        // Lee los datos provenientes de la API
+                        appointment = readTask.Result;
+                    }
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError(string.Empty, "Server error. Please contact an administrator.");
+            }
+
+            return Ok(appointment);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAppointment([FromBody] Appointment appointment)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri($"{API_URL}/api/Appointment/");
+                    var putTask = client.PutAsJsonAsync("PutAppointment/", new {
+                        appointment.Id,
+                        appointment.Status,
+                        appointment.ProfessorComment
+                    });
+
+                    putTask.Wait();
+
+                    var result = putTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var updatedAppointment = await result.Content.ReadFromJsonAsync<Appointment>();
+                        return Json(new
+                        {
+                            success = true,
+                            appointment = updatedAppointment
+                        });
+                    }
+                    else
+                    {
+                        var errorContent = await result.Content.ReadAsStringAsync();
+                        return Json(new
+                        {
+                            success = false,
+                            message = "No se pudo actualizar la cita",
+                            details = errorContent
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        /*
+        public IActionResult CreateNewAppointment([FromBody] Appointment appointment)
+        {
+            try
+            {
+                return Ok(appointmentDAO.CreateAppointment(appointment));
+            }
+            catch (SqlException e)
+            {
+                return StatusCode(500, new { message = "An error occurred", error = e.Message });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = "An unexpected error occurred", error = e.Message });
+            }
+        }
+
+
+
+        public IActionResult GetAppointmentById([FromQuery] string id)
+        {
+            return Ok(appointmentDAO.GetAppointment(id));
+
+
+        }
+
+
+
+        public IActionResult GetAllAppointmentsByUser([FromQuery] string email) {
+
+            return Json(appointmentDAO.GetAll(email));
+
+        }
+        */
+
+    }
+}
